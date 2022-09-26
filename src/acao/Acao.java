a8fab55e3ddefe4d0b6ab8ac625081a8a80cdd85
Pedrao1234 <112420528+Pedrao1234@users.noreply.github.com>
2022-09-26 14:56:58 -0400

    versao1

--- src/acao/Acao.java
+++ src/acao/Acao.java
@@ -6,5 +6,5 @@ public class Acao {
   public final static Mais2 MAIS2 = new Mais2();
   public final static Mais4 MAIS4 = new Mais4();
   public final static TrocaCor TROCACOR = new TrocaCor();
-11
+
 }
