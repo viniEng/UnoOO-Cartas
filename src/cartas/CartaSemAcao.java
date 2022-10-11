ec2ce84fa645e41c63beb8ad71795a0356e6716c
viniEng <108952952+viniEng@users.noreply.github.com>
2022-10-11 17:59:41 -0400

    Create CartaSemAcao.java

---
+++ src/cartas/CartaSemAcao.java
@@ -0,0 +1,37 @@
+package cartas;
+
+public class CartaSemAcao extends Exception  {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CartaSemAcao() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public CartaSemAcao(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CartaSemAcao(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CartaSemAcao(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CartaSemAcao(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
