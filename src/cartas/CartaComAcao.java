7c05d5d051fde13c8a7fbfff6873e9d1379993d1
viniEng <108952952+viniEng@users.noreply.github.com>
2022-10-11 17:56:11 -0400

    Update CartaComAcao.java

--- src/cartas/CartaComAcao.java
+++ src/cartas/CartaComAcao.java
@@ -1,6 +1,14 @@
 package cartas;
 
+/**
+ * Representa a abstração de uma Carta com Ação, ou seja, não possui número
+ * 
+ * @author grupo Cartas
+ *
+ */
 public abstract class CartaComAcao extends Carta {
+	
+	
 
 	// Acao do tipo Acao que vai vir do Grupo Ação:
 	// Muda desde Carta ou altera apenas aqui ?
@@ -15,4 +23,25 @@ public abstract class CartaComAcao extends Carta {
 	public final void setNumero(int n) throws CartaSemNumero {
 		throw new CartaSemNumero("Cartas de Ação não possuem número");
 	}
+	
+	@Override
+	public Acao getAcao()  {
+		return this.acao;
+	}
+	
+	@Override
+	public void setAcao(Acao ac)  {
+		this.acao = ac;
+	}
+
+	@Override
+	public Cor getCor()  {
+		return this.cor;
+	}
+	
+	@Override
+	public void setCor(Cor c)  {
+		 this.cor = c;
+	}
+	
 }
