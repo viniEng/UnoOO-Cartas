871a50047cc26bc349fcbcabe013739b02a820c5
viniEng <108952952+viniEng@users.noreply.github.com>
2022-10-11 17:55:19 -0400

    Update Carta.java

--- src/cartas/Carta.java
+++ src/cartas/Carta.java
@@ -2,31 +2,74 @@ package cartas;
 
 import acao.Acao;
 import acao.Mais2;
+import acao.Mais4;
+import acao.Bloqueio;
+import acao.Inverter;
+import acao.TrocaCor;
+
+
+/**
+ * Representa a abstração de uma Carta, tendo em vista o agrupamento de todos os tipos
+ * 
+ * @author grupo Cartas
+ *
+ */
 
 public abstract class Carta {
-	/**
-	 * Trocar para ação
-	 */
+
+	
 	public final static Acao MAIS2 = new Mais2();
-	public final static String MAIS4 = "+4";
-	public final static String BLOQ = "bloqueio";
-	public final static String INVERTE = "reverso";
-	public final static String TROCACOR = "trocaCor";
+	public final static Acao MAIS4 = new Mais4();
+	public final static Acao BLOQ = new Bloqueio();
+	public final static Acao INVERTE = new Inverter();
+	public final static Acao TROCACOR = new TrocaCor();
 
-	private Cor cor;
-	private int numero;
-	private String acao;
-
+	protected Cor cor;
+	protected int numero;
+	//protected String acao;
+	protected Acao acao;
+	
+    
+	/**
+	 * 
+	 * @return a cor da carta 
+	 */
 	public abstract Cor getCor();
-
-	public abstract String getAcao();
-
+    
+	/**
+	 * 
+	 * @return a ação da carta
+	 * @throws Carta Sem Ação caso uma carta sem ação use o getAcao
+	 */
+	public abstract Acao getAcao() throws CartaSemAcao;
+	
+	/**
+	 * 
+	 * @return o número da carta
+	 * @throws Carta Sem Numero caso uma carta sem numero use o getNumero
+	 */
 	public abstract int getNumero() throws CartaSemNumero;
 
+	/**
+	 * 
+	 * @param c
+	 */
 	public abstract void setCor(Cor c);
 
-	public abstract void setAcao(String ac);
+	/**
+	* @param ac
+	* 
+    *
+	* @throws Carta Sem Acao caso uma carta sem Acao use o setAcao
+	 */
+	public abstract void setAcao(Acao ac) throws CartaSemAcao;
 
+	/**
+	 * @param n
+	 * 
+	 *
+	 * @throws Carta Sem Numero caso uma carta sem numero use o setNumero
+	 */
 	public abstract void setNumero(int n) throws CartaSemNumero;
 
 	@Override
