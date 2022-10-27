f543ad330f2bba1dea87bf71cf9447829d0db72d
Pedrao1234 <112420528+Pedrao1234@users.noreply.github.com>
2022-10-27 10:11:35 -0400

    trocacor

--- src/acao/TrocaCor.java
+++ src/acao/TrocaCor.java
@@ -1,24 +1,20 @@
 package acao;
 import base.Roda;
-/
+/**
  * @author Vinicius 
  * @version 3.5
  * @since 26/10/2022
  */
 
 public class TrocaCor extends Acao {
-  /
+  /**
    * @deprecated 
    *
    *
-   * Na função "realizar", supostamente deve utilizar "jogada.mudarCor", de forma que altere para a cor desejada
-   /
-
-  public void realizar() {
-    //.mudarCor(); /** jogador atual irá escolher para qual cor deseja alterar (apenas exemplo, provavelmente está errado e será modificado)/ 
-  }
+   * Na função "realizar", roda ou jogada ou jogo tem de implementar essa função, de forma que altere para a cor desejada
+   */
   public void realizar (Roda roda) {
-    System.out.println("Você está utilizando os parâmetros errados");
+    //.mudaCor
     return;
   }
 }
