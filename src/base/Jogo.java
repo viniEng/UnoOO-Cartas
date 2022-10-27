33670f4cefc84652c76a643e9c593da32d5c56ae
Luciano Édipo <lucianoedipo@gmail.com>
2022-10-27 18:20:36 -0400

    correção do Jogo
    Retirada do run() no preprarJogo();
    Correção de } no código

--- src/base/Jogo.java
+++ src/base/Jogo.java
@@ -4,12 +4,12 @@
 package base;
 
 import java.util.ArrayList;
-import java.util.Scanner;
-import acao.Acao;
-import base.jogador.Jogador;
-import cartas.Carta;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import acao.Acao;
+import base.jogador.Jogador;
 
 /**
  * @author Autores: Daniel Schutz, Felipe Pellissari, Fernanda Pessoa e José
@@ -26,7 +26,8 @@ public class Jogo {
 	private ArrayList<Acao> acumulo = new ArrayList<>();
 
 	/**
-	 * Prepara o jogo, instanciando o baralho e a roda 
+	 * Prepara o jogo, instanciando o baralho e a roda
+	 * 
 	 * @see Baralho.java
 	 * @see Roda.java
 	 */
@@ -40,14 +41,15 @@ public class Jogo {
 		 * instanciar a roda e mandar baralho e lista de jogadores
 		 */
 		LOGGER.info("Instanciando a roda");
-		this.roda = new Roda(this.baralho, this.jogadores);
+		roda = new Roda(this.baralho, this.jogadores);
 	}
 
-	 /**
-	            *Verifica se a quantida de cartas na mão do jogador é igual a 0
-	            *e define o ganhador se for o caso
-	            *@see Mao.java
-	         */
+	/**
+	 * Verifica se a quantida de cartas na mão do jogador é igual a 0 e define o
+	 * ganhador se for o caso
+	 * 
+	 * @see Mao.java
+	 */
 	public boolean confereFim() {
 		if (baralho.quantCarta() == 0) {
 			return false;
@@ -55,53 +57,44 @@ public class Jogo {
 			return true;
 		}
 	}
-	
+
 	/**
-	* função que faz a execução do jogo
-	*/
+	 * função que faz a execução do jogo
+	 */
 	public void run() {
-		 while(true){
-			
-		    /**
-	            *define o próximo jogador
-	            */
-		    LOGGER.info("Alterando para próximo jogador");		
-	            jogadorAtual = roda.jogadorDaVez();
-		     /**
-	            *Jogador atual realiza a jogada (entra em contato com a roda para ver se há acumulo e decide se compra, joga ou executa as ações acumuladas
-		    *@see Jogador.java
-	            */
-		    LOGGER.info("Jogador realizando jogada");
-	            jogadorAtual.realizarJogada();
-		
-		
-	           
-	           LOGGER.info("Conferindo se acabou as cartas na mão do jogador");
-	            if(confereFim()==false)
-	            {
-	                System.out.printf("O jogador %S ganhou.",jogadorAtual.getNome()); /*printar o jogador que ficou sem cartas na mão*/
-	                break;
-	            }
+		while (true) {
 
-	            
+			/**
+			 * define o próximo jogador
+			 */
+			LOGGER.info("Alterando para próximo jogador");
+			jogadorAtual = roda.jogadorDaVez();
+			/**
+			 * Jogador atual realiza a jogada (entra em contato com a roda para ver se há
+			 * acumulo e decide se compra, joga ou executa as ações acumuladas
+			 * 
+			 * @see Jogador.java
+			 */
+			LOGGER.info("Jogador realizando jogada");
+			jogadorAtual.realizarJogada();
 
-	          }
+			LOGGER.info("Conferindo se acabou as cartas na mão do jogador");
+			if (confereFim() == false) {
+				System.out.printf("O jogador %S ganhou.",
+						jogadorAtual.getNome()); /* printar o jogador que ficou sem cartas na mão */
+				break;
+			}
+
+		}
 	}
+
 	/**
-		 * construtor da classe Jogo
-		 */
-	public Jogo(ArrayList<Jogador> j){
+	 * construtor da classe Jogo
+	 */
+	public Jogo(ArrayList<Jogador> j) {
 		this.jogadores = j;
 		LOGGER.info("Preparando o jogo");
 		prepararJogo();
-		LOGGER.info("Executando o jogo");
-		run();
-		
 
+	}
 }
-
-	
-	
-	
-	
-
