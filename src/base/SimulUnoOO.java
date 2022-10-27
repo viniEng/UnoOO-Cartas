d37145bebf80a0843a37be07b6670abe060129ca
Luciano Édipo <lucianoedipo@gmail.com>
2022-10-27 15:08:14 -0400

    Criando Main e Adicionando lib de Logging
    Foi criado a classe Main que vai rodar a simulação, SimulUnOO.
    
    Adicionado a lib externa slf4j

---
+++ src/base/SimulUnoOO.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Luciano
+ *
+ */
+public class SimulUnoOO {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SimulUnoOO.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		LOGGER.info("Iniciando partida");
+		Jogo partida = new Jogo();
+
+	}
+
+}
