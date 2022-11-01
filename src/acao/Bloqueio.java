dacc45e79349754c8a8a63a98f36687a8a38dd1c
Lucas Ulbrecht <98244995+LucasUlbrecht@users.noreply.github.com>
2022-10-31 22:24:36 -0400

    Bloqueio metodo toString + correção novas ações

--- src/acao/Bloqueio.java
+++ src/acao/Bloqueio.java
@@ -1,16 +1,17 @@
 package acao;
 import base.Roda;
+import cartas.Cor;
 /**
  * @author Lucas Patrizi
  * @version 4.0
- * @since 03/10/22
+ * @since 31/10/22
  * @see Roda
  */
 
 public class Bloqueio extends Acao {
   /**
    * Pula a jogada de um jogador, usando a função pular da roda
-   * (roda.pular()), para o proximo jogador
+   * (roda.pular())
    * 
    * @param roda - é a roda usada no próprio jogo
    * @return
@@ -19,9 +20,37 @@ public class Bloqueio extends Acao {
   public void pular(Roda roda) {
     roda.pular();
     LOGGER.info("Jogador pulado\n");
+    return;
   }
-  @Override
 
+  /**
+   * Informar acesso indevido de função
+   * @param roda
+   */
+  public void inverter (Roda roda){
+    LOGGER.info("Bloqueio não pode inverter\n");
+    return;
+  }
+
+  /**
+   * Informar acesso indevido de função
+   * @param roda
+   */
+  public void comprar (Roda roda){
+    LOGGER.info("Bloqueio não pode comprar\n");
+    return;
+  }
+
+  /**
+   * Informar acesso indevido de função
+   * @return Cor SEMCOR
+   */
+  public abstract Cor trocaCor (){
+    LOGGER.info("Bloqueio não pode trocar cor\n");
+    return Cor.SEMCOR;
+  }
+
+  @Override
   public String toString(){
     return "BLOQUEIO";
   }
