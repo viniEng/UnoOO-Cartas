70d6aada3a10af78c046bf24103b7e3c7a05713c
FernandoFMoreira <113862553+FernandoFMoreira@users.noreply.github.com>
2022-10-31 22:01:51 -0400

    ação atualizada: novas funções abstratas/ abandono do realizar

--- src/acao/Acao.java
+++ src/acao/Acao.java
@@ -1,12 +1,13 @@
 package acao;
 import base.Roda;
+import cartas.*;
 import base.SimulUnoOO;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class Acao {
   protected static final Logger LOGGER = LoggerFactory.getLogger(Acao.class);
+
   /**
    * Função que serve para padronizar a forma dos métodos da ação.
    * Bloqueio pula o próximo jogador da roda.
@@ -14,6 +15,31 @@ public abstract class Acao {
    * @param roda
    */
   public abstract void realizar (Roda roda);
+
+  /**
+   * Função que serve para avançar para o próximo jogador
+   * @param roda
+   */
+  public abstract void pular (Roda roda);
+
+  /**
+   * Função que serve para inverter o sentido da roda
+   * @param roda
+   */
+  public abstract void inverter (Roda roda);
+
+  /**
+   * Função que serve para adicionar n cartas do monte à mão do jogador
+   * @param roda
+   */
+  public abstract void comprar (Roda roda);
+
+  /**
+   * Função que serve para trocar a cor de uma carta pela cor escolhida pelo jogador
+   * 
+   */
+  public abstract Cor trocaCor ();
+
   /**
    * Função que serve para padronizar metódos de cartas especiais sem cor (+4, troca cor)
    * @param roda
@@ -24,7 +50,7 @@ public abstract class Acao {
 
 /**
  * @author Fernando Favaro Moreira
- * @since 26/10/2022
- * @version 4.0
+ * @since 31/10/2022
+ * @version 5.0
  * @see Roda, Jogada
  */
