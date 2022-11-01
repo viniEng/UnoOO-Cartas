c8f633b5754c067929e6b5f676b93e74c87ba3fb
Lucas Ulbrecht <98244995+LucasUlbrecht@users.noreply.github.com>
2022-10-31 22:24:47 -0400

    Correção ação

--- src/acao/Acao.java
+++ src/acao/Acao.java
@@ -9,14 +9,6 @@ public abstract class Acao {
   protected static final Logger LOGGER = LoggerFactory.getLogger(Acao.class);
 
   /**
-   * Função que serve para padronizar a forma dos métodos da ação.
-   * Bloqueio pula o próximo jogador da roda.
-   * Inverter reverte o sentido da roda.
-   * @param roda
-   */
-  public abstract void realizar (Roda roda);
-
-  /**
    * Função que serve para avançar para o próximo jogador
    * @param roda
    */
@@ -36,16 +28,9 @@ public abstract class Acao {
 
   /**
    * Função que serve para trocar a cor de uma carta pela cor escolhida pelo jogador
-   * 
+   * @return Cor - cor escolhida pelo jogador
    */
   public abstract Cor trocaCor ();
-
-  /**
-   * Função que serve para padronizar metódos de cartas especiais sem cor (+4, troca cor)
-   * @param roda
-   * @return Carta
-   */
-  //public abstract void Carta (Roda roda);
 }
 
 /**
