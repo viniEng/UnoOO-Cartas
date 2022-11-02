a51421631e4cce7f906b86e28d8bb19d4cec42a7
Lucas Ulbrecht <98244995+LucasUlbrecht@users.noreply.github.com>
2022-11-02 11:20:55 -0400

    descrição completa troca cor

--- src/acao/Acao.java
+++ src/acao/Acao.java
@@ -21,13 +21,18 @@ public abstract class Acao {
   public abstract void inverter (Roda roda);
 
   /**
-   * Função que serve para adicionar n cartas do monte à mão do jogador
+   * Função que serve para adicionar n cartas do monte à mão do jogador, 
+   * caso chamado por um mais 2 será realizado duas compras para o jogador, e caso chamado por mais 4 realizara quatro compras
    * @param roda
    */
   public abstract void comprar (Roda roda);
 
   /**
-   * Função que serve para trocar a cor de uma carta pela cor escolhida pelo jogador
+   * Troca cor, o método se for chamado para as ações +4 ou troca cor, ira pedir ao usuário 
+   * que informe uma cor entre, amarelo, azul, vermelho ou verde, e ira retornar esta Cor, 
+   * se a ação não for de +4 ou troca cor, ele ira alertar ao usuário que a ação escolhida não consegue mudar 
+   * a cor, caso o usuário não informe uma cor valida o método ira alertar que a cor é invalida e fechara. 
+   * Obs o metodo aceita a cor independente se as letras forem maiusculas ou minusculas
    * @return Cor - cor escolhida pelo jogador
    */
   public abstract Cor trocaCor ();
