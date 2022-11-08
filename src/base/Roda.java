75a1a5865ece794f1ed32ee8ce1b4611e3eb15a9
matheus-nantes <74730431+matheus-nantes@users.noreply.github.com>
2022-11-07 20:45:15 -0400

    Alterações solicitadas pelo professor 07-11
    As principais alterações foram:
    *Detalhamento de comentários;
    *Ajuste na estrutura de verificação de compatibilidade de jogada;
    *"limpeza" da variável "corEscolhida" nos momentos apropriados(Ato de descartar uma carta, ou seja, quando uma jogada é realizada);
    *Inserção da Exceção "JogadaImpossivel", que é lançada quando a carta jogada é incompatível com última carta do descarte. Essa exceção deve ser tratada pelo Jogador.

--- src/base/Roda.java
+++ src/base/Roda.java
@@ -13,22 +13,22 @@ public class Roda {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Roda.class);
 
 	/**
-	 * Baralho de compra.
+	 * Baralho de compra ou seja, de onde as cartas são retiradas e entregues ao jogador.Apenas em dois momento cartas são inseridas nele: Ao iniciar o jogo, e ao "Transformar o descarte em compra"
 	 */
 	private Baralho compra;
 
 	/**
-	 * Baralho de descarte.
+	 * Baralho de descarte, onde as cartas jogadas pelo jogadores são inseridas. O único momento em que as cartas são retiradas deles é quando o método "transformaDescarte" é chamado, para que o monte de compra não fique vazia, as cartas são removidas dele(descarte) e passado para o monte de "compra", mantendo apenas a última carta jogada, que é a determinante da próxima jogada
 	 */
 	private Baralho descarte = new Baralho(Baralho.NORMAL);
 
 	/**
-	 * Arraylist de jogadores.
+	 * Arraylist de jogadores, onde os jogadores armazenados apenas para ter um controle de suas posições "na roda".
 	 */
 	private ArrayList<Jogador> jogadores = new ArrayList<>();
 
   	/**
-	 * Arraylist de acumulo de +2 e +4.
+	 * Arraylist de acumulo das ações de +2 e +4, que como não são ações instantâneas, devem ser guardadas até que possam ser defato realizadas.
 	 */
 	private ArrayList<Acao> acumulo = new ArrayList<>();
   
@@ -38,12 +38,12 @@ public class Roda {
 	private int sentido = 1;
 
 	/**
-	 * Posicao atual da roda.
+	 * Posicao do jogador na roda que deve realizar a jogada do momento.
 	 */
 	private int posicaoAtual =  0;
 
 	/**
-	 * Variável que armazena momentaneamente a cor recebida em "trocarCor"
+	 * Variável que armazena momentaneamente a cor recebida em "trocarCor".Inicializada em zero, porque não é possível o jogo começar com um +4 ou "escolhe cor"
 	 */
 	private Cor corEscolhida = null;
 
@@ -101,8 +101,8 @@ public class Roda {
 		this.jogadores = jogadoresRecebidos;
 		LOGGER.info("Há {} Jogadores na roda", jogadoresRecebidos.size());
 		LOGGER.info("Iniciando distribuição de cartas iniciais");
-		for (int i = 0; i < this.jogadores.size(); i++) {
-			for (int j = 0; j < 7; j++) {
+		for (int i = 0; i < this.jogadores.size(); i++) {/*para cada jogador... */
+			for (int j = 0; j < 7; j++) {/*...distribui 7 cartas */
 				Carta cartaAux = entregarCarta();
 				Jogador jogadorAux = this.jogadores.get(i);
 				jogadorAux.comprar(cartaAux);
@@ -116,67 +116,113 @@ public class Roda {
 			this.descarte.receberCarta(cartaAux);
 			if(cartaAux instanceof CartaEspecialSemCor || cartaAux instanceof CartaEspecialComCor)
 				LOGGER.info("{} foi a carta retirada, comprando mais uma", cartaAux);
-		} while (cartaAux instanceof CartaEspecialSemCor || cartaAux instanceof CartaEspecialComCor);
+		} while (cartaAux instanceof CartaEspecialSemCor || cartaAux instanceof CartaEspecialComCor);/*caso a carta não possuir cor ou número, ou seja, for uma instância da classe "CartaEspecialSemCor", devemos pegar outra carta para que o primeiro jogador possa fazer sua jogada */
 		LOGGER.info("{} é a primeira carta do jogo", cartaAux);
 
 	}
 
 	/**
-	 * Verifica a carta jogada e se for compatível, insere uma carta no monte de descarte.
+	 * Verifica a carta jogada e se for compatível, insere uma carta no monte de descarte, caso contrário, lança uma exceção.
 	 * @param recebida Carta recebida para inserir no monte de descarte.
 	 */
-	public void descartarCarta(Carta recebida) {
-		LOGGER.info("Descartando carta: {}", recebida);
-		Carta ultima = getUltimaCarta();
-		int nRecebida = -1;
-		int  nUltima = -1;
-		Acao aRecebida = null
-		Acao aUltima = null;
-		Cor cRecebida = recebida.getCor();
-		Cor cUltima = ultima.getCor();
+	public void descartarCarta(Carta recebida) throws JogadaImpossivel{
+		Carta ultima = getUltimaCarta();/*Ultima carta jogada no monte de descarte */
+		int nRecebida = -1;/*nuemração da jogada atual, inicializado com -1 pois existem cartas com o número 0 */
+		int  nUltima = -1;/*numeração da ultima carta jogada no monte de descarte */
+		Acao aRecebida = null;/*Ação da carta da jogada atual*/
+		Acao aUltima = null;/*Ação da última carta do monte de descarte */
+		Cor cRecebida = recebida.getCor();/*Cor da carta da jogada atual */
+		Cor cUltima = ultima.getCor();/*Ação da última carta jogada no monte de descarte */
+		boolean status = false;/*variável que é uma representação se a jogada foi realizada de fato, ou seja, se a carta é compatível. Caso ela se torne verdadeira, podemos zerar a "cor temporária - corEscolhida" e notificar, através do LOGGER, que a jogada foi realizada*/
 			
 		try{
-			nRecebida = recebida.getNumero();
-		}catch (CartaSemNumero a){
+			nRecebida = recebida.getNumero();/*tentamos pegar  o número da carta da jogada */
+		}catch (CartaSemNumero a){/*se não for possível, tratamos a exceção */
 			LOGGER.trace("a carta {} não possui número", recebida);
 		}
-		try{
+		try{/*tentamos pegar a ação da carta da jogada  */
 			aRecebida = recebida.getAcao();
-		}catch (CartaSemAcao b){
+		}catch (CartaSemAcao b){/*se não for possível, tratamos a exceção */
 			LOGGER.trace("a carta {} não possui ação", recebida);
 		}
 
-		try{
+		/*Optamos por sempre atualizarmos os valores da última carta do monte, primeiro, para que nenhuma informação seja perdida(no caso da "mudança de cor"),segundo, para envitar possível conflitos na primeira jogada do jogo, e terceiro, para não termo que criar essa variáveis na classe, sendo que ela são acessadas apenas por este método específico, que é apenas durante a verificação */
+
+		try{/*tentamos pegar o número da última carta do monte de descarte  */
 			nUltima = ultima.getNumero();
-		}catch (CartaSemNumero a){
+		}catch (CartaSemNumero a){/*se não for possível, tratamos a exceção */
 			LOGGER.trace("a última carta do descarte {} não possui número", ultima);
 		}
-		try{
+		try{/*tentamos pegar a ação da última carta do monte de descarte  */
 			aUltima = ultima.getAcao();
-		}catch (CartaSemAcao b){
+		}catch (CartaSemAcao b){/*se não for possível, tratamos a exceção */
 			LOGGER.trace(" última carta do descarte {} não possui ação", ultima);
 		}
 
-		if(aRecebida == Carta.MAIS4){
+		/*
+		 * Se a carta for um mais 4, ele pode ser jogada a qualquer momento, exceto se a ultima carta jogada acumuluda foi um +4
+		 */
+		if(aRecebida == Carta.MAIS4 && acumulo.get(acumulo.size() - 1) != Carta.MAIS4){
 			this.descarte.receberCarta(recebida);
 			acumulo.add(aRecebida);
-			corEscolhida = null;
+			status = true;
 		}
+
+		/*
+		 * se a carta for um Escolhe Cor, ela pode ser jogada a qualquer momento, exceto se houver acúmulo
+		 */
 		else if(aRecebida == Carta.TROCACOR && temAcumulo() == false){
 			this.descarte.receberCarta(recebida);
-			corEscolhida = null;
+			status = true;
 		}
-		else if(nRecebida == nUltima || cRecebida == cUltima || aUltima == aRecebida){
+
+		/*
+		 * se a carta for numérica, e tiver o mesmo número da última carta vigente, ela pode ser jogada
+		 */
+		else if(nRecebida == nUltima && nRecebida != -1){
 			this.descarte.receberCarta(recebida);
-			corEscolhida = null;
+			status = true;
+		}
+
+		/*
+		 * se a carta tiver cor, e tiver a mesma cor da última carta vigente, ela pode ser jogada
+		 */
+		else if(cRecebida == cUltima && cRecebida != Cor.SEMCOR){
+			this.descarte.receberCarta(recebida);
+			status = true;
+		}
+
+		/*
+		 * se a carta possuir ação, e tiver a mesma ação da última carta vigente, ela pode ser jogada
+		 */
+		else if(aRecebida == aUltima && aRecebida != null){
+			this.descarte.receberCarta(recebida);
+			status = true;
 			if(aRecebida == Carta.MAIS2){
 				acumulo.add(aRecebida);
 			}
 		}
+
+		/*
+		 * se a carta não for compatível, uma exceção deev ser lançada para que o jogador tome alguma providência
+		 */
+		else{
+			throw new JogadaImpossivel();
+		}
+
+		/*
+		 * Se alguma carta foi jogada, podemos zerar a cor temporária, e informamos que esta ação de jogar a carta foi realizada com sucesso
+		 */
+		if(status){
+			corEscolhida = null;
+			LOGGER.info("A carta {} foi descartada", recebida);
+		}
+
 	}
 
+
 	/**
-	 * Compra uma carta do baralho de compra.
+	 * Compra uma carta do baralho de compra. Caso o baralho de compra possua nenhuma carta, realizamos a ação de "transformar" o barralho de descarte no de compra
 	 * @return Primeira carta do monte de compra.
 	 */
 	public Carta entregarCarta() {
@@ -190,11 +236,11 @@ public class Roda {
 	}
 
 	/**
-	 * Transforma o baralho de descarte no baralho de compra.
+	 * Transforma o baralho de descarte no baralho de compra, deixando apenas a última carta,que é a carta determinante da próxima jogada.
 	 */
 	public void transformaDescarte() {
 		LOGGER.info("Transformando monte de descarte em monte de compra");
-		for (int i = 0; i < this.descarte.getCartas().size() - 1; i++) {
+		for (int i = 0; i < this.descarte.getCartas().size() - 2; i++) {
 			this.compra.receberCarta(this.descarte.comprarCarta());
 		}
 		this.compra.embaralhar();
@@ -209,17 +255,16 @@ public class Roda {
 	}
 
 	/**
-	 * Dobra o sentido.
+	 * Aumenta o incremento de posição, ou seja, ao invés de passar para a próxima posição, +1, ele passa para a próxima da próxima, +2. Ao selecionar o próximo jogador, esse valor é alterado para seu valor original, mantendo o fluxo comum do jogo.
 	 */
 	public void pular() {
 		this.sentido *= 2;
-		LOGGER.info("Sentido dobrado: {}", this.sentido);
 	}
 
 	/**
 	 * Determina qual é o jogador responsavel por jogar no turno.
 	 * @param jogadorRecebido Jogador
-	 * @return Posição do jogador na roda
+	 * @return Qual jogador que deve realizar a jogada
 	 */
 	public Jogador jogadorDaVez() {
 		LOGGER.info("O jogador da vez é {}", this.jogadores.get(posicaoAtual));
@@ -231,17 +276,19 @@ public class Roda {
 	 */
 	public void proximoJogador(){
 		posicaoAtual = (this.posicaoAtual + this.sentido) % this.jogadores.size();
-		if(sentido%2==0){
-			LOGGER.info("Sentido voltou ao normal");
-			this.sentido/=2;
+		if(sentido%2==0){/*caso um jogador tenha sido pulado anteriormente... */
+			LOGGER.info("Um foi jogador pulado");
+			this.sentido/=2;/*...retorna o incremento ao seu valor original para voltar o jogo ao fluxo comum */
 		}
 	}
 
 	/**
-	 * altera a cor armazenada
+	 * @param Cor determinada pelo jogador, recebida através da ação
+	 * Recebe uma cor e a armazena temporariamente (até que alguma carta seja descartada ou comprada)
 	 */
 	public void mudarCor(cor recebida) {
 		corEscolhida = recebida;
+		LOGGER.info("A cor escolhida foi: {}",corEscolhida);
 	}
 
 	/**
@@ -250,18 +297,18 @@ public class Roda {
 	public Carta getUltimaCarta() {
 		Carta cartaAux = this.descarte.ultimaCarta();
 		if (this.corEscolhida == null){
-			LOGGER.info("A ultima carta jogada foi {}", cartaAux);
+			LOGGER.trace("A ultima carta jogada foi {}", cartaAux);
 			return cartaAux;
 		}
 		else{
-			LOGGER.info("A ultima carta jogada foi {} e a cor escolhida foi{}", cartaAux,corEscolhida);
+			LOGGER.trace("A ultima carta jogada foi {} e a cor escolhida foi{}", cartaAux,corEscolhida);
 			cartaAux.setCor(corEscolhida);
 			return cartaAux;
 		}
 	}
 
 	/**
-	 * Compra um numero de cartas e entrega a um jogador.
+	 * Compra um numero de cartas e entrega a um jogador. Foi criada com o intuito de faciliar quando houver a necessidade de comprar cartas acumuladas, e que devem ser atribuídas a um jogador específico
 	 * @param qtd Quantidade de cartas a serem compradas.
 	 * @param jogador Jogador que vai comprar.
 	 */
@@ -270,11 +317,12 @@ public class Roda {
 		for (int i = 0; i < qtd; i++) {
 			jogador.comprar(entregarCarta());
 		}
+		corEscolhida = null;
 	}
   
   /**
-	 * salva o acumulo em um ArrayList auxiliar, limpa o acumulo, e retorna o auxiliar
-	 * @return Acumulo de ações
+	 * salva o acumulo em um ArrayList auxiliar, limpa o acumulo, e retorna o auxiliar que contém as ações que devem ser executadas
+	 * @return Acumulo de ações que devem ser executadas
 	 */
   public ArrayList<Acao> desacumular(){
     ArrayList<Acao> acumuloAux = this.acumulo;
@@ -313,5 +361,14 @@ public class Roda {
 		return "Roda: Posição atual: " + this.posicaoAtual + ", Sentido: " + sent 
 			   + ", Num cartas compra: " + this.compra.quantCarta() + ", Num cartas descarte: " 
 			   + this.descarte.quantCarta();
+	}
+}
+
+/*Exceção gerada quando um jogada é incorreta, ou seja, a carta jogada não é compatível com a última carta do monte de descarte. Teoricamente, esta exceção retorna ao jogador, que deve tomar alguma providência a respeito */
+/*Dúvida: Não seria mais correto retornar a carta ao jogador, e então lançar a exceção? que é o que de fato acontece no jogo.Aí no caso o jogador tomaria a decisão de jogar uma jogada apropriada, seja compra ou descarte */
+public class JogadaImpossivel extends Exception{
+	@Override
+	public String getMessage(){
+		return "Jogada impossível";
 	}
 }
