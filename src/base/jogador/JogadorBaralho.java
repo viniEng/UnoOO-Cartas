d74ca9f03ddb07e89ba1322000e2fdba2bd8605d
jecelen <112420534+jecelen@users.noreply.github.com>
2022-11-18 23:18:41 -0400

    Update JogadorBaralho.java

--- src/base/jogador/JogadorBaralho.java
+++ src/base/jogador/JogadorBaralho.java
@@ -1,9 +1,14 @@
 package base.jogador;
+
+import acao.Acao;
+import base.Jogo;
+import cartas.Carta;
+import cartas.CartaComAcao;
+import cartas.CartaSemAcao;
+
 /**
  * Subclasse da superclasse 'Jogador' que representa o jogador criado pelo grupo baralho
  * 
- * @author Jecelen Adriane Campos e Guilherme Bispo Cupertino. Grupo Baralho.
- *
  */
 public class JogadorBaralho extends Jogador{
 	/**
@@ -14,5 +19,56 @@ public class JogadorBaralho extends Jogador{
 		super(nome);
 		LOGGER.info("JogadorBaralho criado com sucesso\n");
 	}
-	
+ /**
+  * Recebe como parametro a última jogada realizada e retorna a jogada a ser realizada pelo JogadorBaralho
+  * @param jogadaRealizada
+  * @return a jogada do JogadorBaralho.
+  */
+	public Jogada realizarJogada(Jogada jogadaRealizada){
+        LOGGER.trace("Jogador {} realizando jogada", this.getNome());
+        Carta carta = null;
+        if(Jogo.roda.temAcumulo()){
+            try{
+                carta = defineCartaParaAcumulo(Jogo.roda.getUltimaCarta().getAcao());
+            }catch (CartaSemAcao e) {
+                LOGGER.error("Erro ao tentar definir carta de acúmulo: {}", e);
+            }
+
+            if(carta == null){
+                LOGGER.trace("Jogador {} comprando acúmulo", this.getNome());
+                comprarCartasAcumuladas(Jogo.roda.desacumular());
+                jogadaRealizada = Jogada.COMPRAR_ACUMULADO;
+                LOGGER.info("Jogador {} comprou acúmulo. Ficou com {} cartas", this.getNome(), this.getQuantidadeCartas());
+
+            }else{
+                descartar(carta);
+                LOGGER.info("Jogador {} descartou {} para o acúmulo", this.getNome(), carta.toString());
+                jogadaRealizada = Jogada.DESCARTAR;
+            }
+        }else{
+            carta = defineCartaDaJogada();
+
+            if(carta != null){
+                if(carta instanceof CartaComAcao){
+                    try {
+                        Acao acaoCarta = carta.getAcao();
+                            realizarAcaoDaCarta(acaoCarta);
+                    } catch (CartaSemAcao e) {
+                        LOGGER.error("ERRO: Carta não possui acao!");
+                    }
+                }
+                descartar(carta);
+                jogadaRealizada = Jogada.DESCARTAR;
+
+            }else{
+                LOGGER.info("Jogador {} precisou comprar uma carta", this.getNome());
+            	Jogo.roda.comprar(1, this);
+                jogadaRealizada = Jogada.COMPRAR;
+            }
+        }
+        LOGGER.info("Jogada do JogadorBaralho realizada\n");
+        return jogadaRealizada;
+		
+	}
 }
+
